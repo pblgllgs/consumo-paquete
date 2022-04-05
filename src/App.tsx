de1267bbdb12b1844022204c1cb656de4b3c4484
pblgllgs <pbl.gllgs@gmail.com>
2022-04-04 22:59:54 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Button, MyLabel } from 'pblgllgs-my-storybook-components';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <h1>Hola mundo!!!</h1>
+      <Button size="large" primary label={'Hola mundo'} />
+      <MyLabel
+        allCaps
+        label="Este es mi label personalizado"
+        size="h1"
+        fontColor="pink"
+        color="primary"
+      />
     </div>
   );
 }
